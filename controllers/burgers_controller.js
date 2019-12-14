b0077402a0794328049e58e575e44d74b34e17f9
Grayson M <grayson.mcmurry23@gmail.com>
2019-12-14 00:09:05 -0600

    adding handlebars html

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,7 +3,11 @@ var router = express.Router();
 var burger = require("../models/burger.js");
 
 router.get("/", function(req, res) {
-  burger.all(function(data) {
+  res.redirect("/index");
+});
+
+router.get("/", function(req, res) {
+  burger.selectAll(function(data) {
     var hbsObject = {
       burger: data
     };
@@ -11,54 +15,16 @@ router.get("/", function(req, res) {
     res.render("index", hbsObject);
   });
 });
-router.post("/api/burgers", function(req, res) {
-  burger.create(
-    ["name", "devoured"],
-    [req.body.name, req.body.devoured],
-    function(result) {
-      // Send back the ID of the new quote
-      res.json({ id: result.insertId });
-    }
-  );
+router.post("/burger/create", function(req, res) {
+  burger.insertOne(["name"], [req.body.burger_name], function(result) {
+    // Send back the ID of the new quote
+    res.json({ id: result.insertId });
+  });
 });
-router.put("/api/burgers/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
-
-  console.log("condition", condition);
-
-  burger.update(
-    {
-      devoured: req.body.devoured
-    },
-    condition,
-    function(result) {
-      if (result.changedRows == 0) {
-        // If no rows were changed, then the ID must not exist, so 404
-        return res.status(404).end();
-      } else {
-        res.status(200).end();
-      }
-    }
-  );
-});
-router.delete("/api/burgers/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
-
-  console.log("condition", condition);
-
-  burger.delete(
-    {
-      delete: req.body.delete
-    },
-    condition,
-    function(result) {
-      if (result.changedRows === 0) {
-        return res.status(404).end();
-      } else {
-        res.status(200).end();
-      }
-    }
-  );
+router.post("/burger/eat/:id", function(req, res) {
+  burger.updateOne(req.params.id, function() {
+    res.redirect("/index");
+  });
 });
 
 // Export routes for server.js to use.
