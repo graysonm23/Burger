b11ae548f7519951e875a3d3cad516b0e2e997ba
Grayson M <grayson.mcmurry23@gmail.com>
2020-01-29 13:15:06 -0600

    adding new schema

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,15 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  host: "localhost",
+  host: "us-cdbr-iron-east-04.cleardb.net",
   port: 3306,
-  user: "root",
-  password: "",
-  database: "burgers_db"
+  user: "b3fed2baff7628",
+  password: "c5aafa8c",
+  database: "heroku_f1f44e32c5c60b7"
 });
+//b3fed2baff7628:c5aafa8c@us-cdbr-iron-east-04.cleardb.net/heroku_f1f44e32c5c60b7?reconnect=true
 
-
-connection.connect(function(err) {
+mysql: connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
--- db/schema.sql
+++ db/schema.sql
@@ -1,13 +1,18 @@
-CREATE DATABASE IF NOT EXISTS burgers_db;
+CREATE DATABASE
+IF NOT EXISTS heroku_f1f44e32c5c60b7;
 USE burgers_db;
 
 -- If the table already exists, remove it before trying to create the table again
 DROP TABLE IF EXISTS burgers;
 
 -- Create the burgers table
-CREATE TABLE burgers (
-    id int NOT NULL AUTO_INCREMENT,
-    burger_name varchar(255) NOT NULL,
+CREATE TABLE burgers
+(
+    id int NOT NULL
+    AUTO_INCREMENT,
+    burger_name varchar
+    (255) NOT NULL,
     devoured BOOL DEFAULT false,
-    PRIMARY KEY (id)
+    PRIMARY KEY
+    (id)
 );
